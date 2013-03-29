cddb3a621cebf9ee8ab7ade63c249f51d44cf268
nasmile <linahappy90@163.com>
2013-03-29 15:28:35 +0800

    jquery_position

--- jQuery/js/position.js
+++ jQuery/js/position.js
@@ -1,16 +1,59 @@
-(function($){
-	$.position = function(sid){
-		var top = (window.innerHeight - document.getElementById(sid).clientHeight)/2;
-		var height = (window.innerWidth - document.getElementById(sid).clientWidth)/2;
+(function($) {
+	//对jQuery添加方法
+	// $.fn.extend ({
+	// 	position: function(pinner){
+	// 		var pre = this;
+	// 		var childpre = pre.find(pinner);
+	// 		var top = (pre.height() - childpre.height()) / 2;
+	// 		var left = (pre.width() - childpre.width()) / 2;
 
-		window.onresize = resize;
+	// 		childpre.css({
+	// 			'top': top,
+	// 			'left': left
+	// 		});
+	// 	}
+	// });
 
-		function resize(){
-			$(sid).css({
+	//添加jQuery本身的方法
+	$.extend({
+		position : function(pre,pinner,pos){
+			var pre = $(pre);
+			var childpre = pre.find(pinner);
+			var top ,left;
+
+			if (pos == "center") {
+				top = (pre.height() - childpre.height()) / 2;
+				left = (pre.width() - childpre.width()) / 2;
+			}else if (pos == "tl"){
+				top = 0;
+				left = 0;
+			}else if(pos == "ct"){
+				top = 0;
+				left= (pre.width() - childpre.width()) / 2;
+			}else if (pos == "tr") {
+				top = 0;
+				left = pre.width() - childpre.width();
+			}else if(pos == "bl"){
+				top = pre.height() - childpre.height();
+				left = 0;
+			}else if(pos == "bc"){
+				top = pre.height() - childpre.height();
+				left = (pre.width() - childpre.width()) / 2;
+			}else if (pos == "br") {
+				top = pre.height() - childpre.height();
+				left = pre.width() - childpre.width();
+			}else if(pos == "cl"){
+				top = (pre.height() - childpre.height()) / 2;
+				left = 0;
+			}else if(pos == "cr"){
+				top = (pre.height() - childpre.height()) / 2;
+				left = pre.width() - childpre.width();
+			}
+
+			childpre.css({
 				'top': top,
-				'left': height
+				'left': left
 			});
 		}
-		resize();
-	}
-})(jQuery);
+	});
+})(window.jQuery)
